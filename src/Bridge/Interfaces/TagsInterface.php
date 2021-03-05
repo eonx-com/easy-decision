bbf1a94a6d155b6789505bfcf263eb3d196b9fae
natepage <nathan.page@eonx.com>
2021-03-05 02:47:40 +0000

    Release 2.5.32

---
+++ src/Bridge/Interfaces/TagsInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EonX\EasyDecision\Bridge\Interfaces;
+
+interface TagsInterface
+{
+    /**
+     * @var string
+     */
+    public const DECISION_CONFIGURATOR = 'easy_decision.decision_configurator';
+
+    /**
+     * @var string
+     */
+    public const DECISION_RULE = 'easy_decision.rule';
+}
